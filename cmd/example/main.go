b2d4b052f3185faea69cd3c5a16d1cd8876b6f06
Naofumi Okamoto <nokamoto.engr@gmail.com>
2022-06-09 07:35:44 +0000

    Add crane.Append and crane.Pull examples

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -4,12 +4,21 @@ import (
 	"log"
 	"os"
 
+	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/crane"
 )
 
 func main() {
 	src := os.Getenv("SRC")
+	newTag := os.Getenv("NEW_TAG")
 	var opts []crane.Option
+
+	cfg := authn.AuthConfig{
+		Username: os.Getenv("GITHUB_USER"),
+		Password: os.Getenv("AUTH"),
+	}
+
+	opts = append(opts, crane.WithAuth(authn.FromConfig(cfg)))
 
 	img, err := crane.Pull(src, opts...)
 	if err != nil {
@@ -21,4 +30,25 @@ func main() {
 		log.Fatalf("err Pull: %s: %s", src, err)
 	}
 	log.Printf("Pull: %s", digest)
+
+	if len(os.Args) > 1 {
+		paths := os.Args[1:]
+		img, err = crane.Append(img, paths...)
+
+		if err != nil {
+			log.Fatalf("err Append: %s: %s", src, err)
+		}
+	
+		digest, err := img.Digest()
+		if err != nil {
+			log.Fatalf("err Append: %s: %s", src, err)
+		}
+		log.Printf("Append: %s", digest)
+
+		if err := crane.Push(img, newTag, opts...); err != nil {
+			log.Fatalf("err Push: %s: %s: %s", src, newTag, err)
+		}
+
+		log.Printf("Push: %s: %s", src, newTag)
+	}
 }
